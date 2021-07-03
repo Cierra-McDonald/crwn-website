a49bd3112fcedd9a0908dfd819f6ca1ceee3ca2d
Cierra-McDonald <cierralauren15@gmail.com>
2021-07-03 14:12:04 -0700

    created fake product page

---
+++ src/services/ProductData.js
@@ -0,0 +1,67 @@
+import Product1 from '../images/Product1.jpg';
+import Product2 from '../images/Product2.jpg';
+import Product3 from '../images/Product3.jpg';
+import Product4 from '../images/Product4.jpg';
+
+export const Products = [
+    {
+        name: 'Monoi Deep Conditioner',
+        description: ' 95% Less Breakage',
+        image: Product1,
+        price: '$22.00',
+    },
+    {
+        name: 'Manuka Honey Hair Mask',
+        description: 'For Dry, Brittle & Textured Hair',
+        image: Product2,
+        price: '$10.50',
+    },
+    {
+        name: 'Monoi Sulfate Free Shampoo',
+        description: 'Moisturize & Cleanse',
+        image: Product3,
+        price: '$14.00',
+    },
+    {
+        name: 'Coconut & Creme Restorative Co-Wash',
+        description: 'Revitalize dry curls',
+        image: Product3,
+        price: '$25.00',
+    },
+    {
+        name: 'Coconut & Creme Curl Creme',
+        description: 'Bouncy, Weightless Curls',
+        image: Product1,
+        price: '$17.00',
+    },
+    {
+        name: 'Coconut & Creme Detangler',
+        description: 'Less Frizz, Tangle-free',
+        image: Product1,
+        price: '$7.00',
+    },
+    {
+        name: 'Almond and Castor Oil Hair Blend',
+        description: 'Strengthens weak strands',
+        image: Product4,
+        price: '$8.00',
+    },
+    {
+        name: 'Black Vanilla Hair Oil',
+        description: 'For stronger hair',
+        image: Product4,
+        price: '$13.00',
+    },
+    {
+        name: 'Jojoba Oil Growth Serum',
+        description: 'Thick, long hair',
+        image: Product4,
+        price: '$24.00',
+    },
+    {
+        name: 'Jojoba Hair Mask',
+        description: 'Revitalize dry curls',
+        image: Product2,
+        price: '$28.00',
+    },
+]
